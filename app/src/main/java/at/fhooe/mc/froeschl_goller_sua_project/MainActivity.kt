c468cb9a021c0f5dd1c60063a16c961740a0eff8
Simon Goller <simon.goller4@gmail.com>
2021-12-16 17:54:05 +0100

    Initial commit

---
+++ app/src/main/java/at/fhooe/mc/froeschl_goller_sua_project/MainActivity.kt
@@ -0,0 +1,29 @@
+package at.fhooe.mc.froeschl_goller_sua_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import at.fhooe.mc.froeschl_goller_sua_project.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.activity_main_bottom_navigation)
+     //   val navController = findNavController(binding.)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
